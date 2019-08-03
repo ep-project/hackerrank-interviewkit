9091e21ebc8a898ea76c56a7847a3c346f3a2888
FrancisK <50886774+kapingafrancis@users.noreply.github.com>
2019-08-03 22:22:01 +0100

    Create sockMerchant.java

---
+++ sockMerchant.java
@@ -0,0 +1,20 @@
+public class Merchant {
+
+    static int sockMerchant(int n, int[] ar) {
+        int socks [] = ar;
+        Arrays.sort(socks);//Sort the socks array
+
+        int i = 1;
+        int pairs = 0;
+        while(i < n){
+            if (socks[i] == socks[i-1]){
+                pairs++;
+                i++;
+            }
+            i++;
+        }
+
+        return pairs;
+
+    }
+   }
